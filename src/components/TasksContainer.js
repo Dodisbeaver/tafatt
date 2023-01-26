876ee27146506a73a9a93a40304afb2f1d9a5871
Dodisbeaver <71547325+Dodisbeaver@users.noreply.github.com>
2023-01-26 16:50:55 +0200

    adds dragging but also dropping bug

--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -34,32 +34,57 @@ const TasksContainer = ({ socket }) => {
 		});
 	};
 	return (
+      
 		<div className='container'>
+            <div>
+            <button
+        type="button"
+        // onClick={() => {
+        //   setState([...state, []]);
+        // }}
+      >
+        Add new group
+      </button>
+      <button
+        // type="button"
+        // onClick={() => {
+        //   setState([...state, getItems(1)]);
+        // }}
+      >
+        Add new item
+      </button>
+            </div>
 			<DragDropContext onDragEnd={handleDragEnd}>
 				{Object.entries(tasks).map((task) => (
 					<div
 						className={`${task[1].title.toLowerCase()}__wrapper`}
-						key={task[1].title}
+						key={task[1].id}
 					>
 						<h3>{task[1].title} Tasks</h3>
 						<div className={`${task[1].title.toLowerCase()}__container`}>
-							<Droppable droppableId={task[1].title}>
+							<Droppable droppableId={task[1].id} key={task[1].id} >
 								{(provided) => (
 									<div ref={provided.innerRef} {...provided.droppableProps}>
 										{task[1].items.map((item, index) => (
+                                            
 											<Draggable
+                                            
 												key={item.id}
 												draggableId={item.id}
 												index={index}
 											>
+                                                
 												{(provided) => (
 													<div
 														ref={provided.innerRef}
 														{...provided.draggableProps}
 														{...provided.dragHandleProps}
+                                                        
 														className={`${task[1].title.toLowerCase()}__items`}
+                                                        
 													>
 														<p>{item.title}</p>
+                                                        <p>{index}</p>
 														<p className='comment'>
 															<Link
 																to={`/comments/${task[1].title}/${item.id}`}
